c87e64a300fd6ff62667a756f4b0068bbb37b501
Jatanael <jatanael.ods@gmail.com>
2021-11-14 18:05:20 -0300

    2.0

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
@@ -15,6 +15,8 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score == winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
@@ -25,19 +27,26 @@ p2Button.addEventListener('click', function () {
         p2Score += 1;
         if (p2Score == winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
     }
 })
 
 winningScoreSelect.addEventListener('change', function () {
-    alert("CHANGEEEE!!")
+    winningScore = parseInt(this.value);
+    reset();
 })
 
-resetButton.addEventListener('click', function () {
+resetButton.addEventListener('click', reset)
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-})
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
